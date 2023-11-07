41ea1321bdd3df3728fb3284dc158f256fa06e37
Louis Pallett <louis.pallett@outlook.com>
2023-11-07 13:30:54 +0000

    Updated caesarCipher() to include encryption code parameter

--- scripts/caesarCipher.js
+++ scripts/caesarCipher.js
@@ -1,16 +1,36 @@
-const caesarCipher = (str) => {
+const caesarCipher = (str, enCode) => {
+
+    if(enCode > 25 || enCode < 1) {
+        return "Encryption code must between (and including) 1 and 25."
+    }
+
     const result = [];
     
     for(let i = 0; i < str.length; i++) {
         if(isAlphanumeric(str.charAt(i))) {
             const letter = str.charAt(i);
-            if(letter == "z") {
-                result.push("a");
-            } else {
-                const ascii = letter.charCodeAt(0) + 1;
-                const newLetter = String.fromCharCode(ascii);
-                result.push(newLetter);
+            const ascii = letter.charCodeAt(0) + enCode;
+            let newLetter;
+            // determine if upper or lower case
+            if(str.charAt(i) == str.charAt(i).toUpperCase()) {
+                // determine whether ascii is valid
+                if(ascii > 90) {
+                    // if invalid, calculate how much over 90 it is, take that number, and add 64 (the value before 'A')
+                    const overBy = ascii - 90;
+                    newLetter = String.fromCharCode(overBy + 64);
+                } else {
+                    newLetter = String.fromCharCode(ascii);
+                }
+            } else if(str.charAt(i) === str.charAt(i).toLowerCase()) {
+                // if lower case... (96 is the value before 'a')
+                if(ascii > 122) {
+                    const overBy = ascii - 122;
+                    newLetter = String.fromCharCode(overBy + 96);
+                } else {
+                    newLetter = String.fromCharCode(ascii);
+                }
             }
+            result.push(newLetter);
         } else {
             result.push(str.charAt(i));
         }
--- scripts/caesarCipher.test.js
+++ scripts/caesarCipher.test.js
@@ -1,17 +1,29 @@
 import caesarCipher from "./caesarCipher";
 
 test("Encrypts one word", () => {
-    expect(caesarCipher("hello")).toBe("ifmmp");
+    expect(caesarCipher("hello", 1)).toBe("ifmmp");
 });
 
 test("Encrypts multiple words", () => {
-    expect(caesarCipher("hello there")).toBe("ifmmp uifsf");
+    expect(caesarCipher("hello there", 5)).toBe("mjqqt ymjwj");
 });
 
-test("Wraps from a to z", () => {
-    expect(caesarCipher("zoo")).toBe("app");
+test("Wraps from a to z (lowercase)", () => {
+    expect(caesarCipher("zoo", 10)).toBe("jyy");
+})
+
+test("Wraps from a to z (uppercase)", () => {
+    expect(caesarCipher("ZOO", 10)).toBe("JYY");
 })
 
 test("Ignores punctuation", () => {
-    expect(caesarCipher("hello, there!")).toBe("ifmmp, uifsf!");
+    expect(caesarCipher("hello, there!", 1)).toBe("ifmmp, uifsf!");
+});
+
+test("Returns error when enCode > 25", () => {
+    expect(caesarCipher("Wallop", 26)).toBe("Encryption code must between (and including) 1 and 25.");
+});
+
+test("Returns error when enCode < 1", () => {
+    expect(caesarCipher("Whatever", -1)).toBe("Encryption code must between (and including) 1 and 25.");
 });
